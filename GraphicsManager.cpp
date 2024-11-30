a60fabce6816d7273f9a33a91b809ed79c062752
Babos Csaba <babos.csaba@yourweb.hu>
2024-11-30 22:53:08 +0100

    Continuing setting up vulkan, build is working

--- GraphicsManager.cpp
+++ GraphicsManager.cpp
@@ -1,4 +1,6 @@
+#include <cstdint>
 #include <iostream>
+#include <stdexcept>
 #include <vulkan/vulkan_core.h>
 using namespace std;
 
@@ -17,80 +19,155 @@ GraphicsManager::GraphicsManager(int width, int height, const char* title) {
 
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // No OpenGL please 
 
-	if (!CreateVulkanInstance()) {
-		glfwTerminate();
-		return;
-	}
-
 	cout << "Creating new window [" << width << "x" << height << " | " 
-		 << title << endl;
+		<< title << endl;
 
 	window = glfwCreateWindow(width, height, title, nullptr, nullptr);
 	if (!window) {
 		cerr << "Failed to create window" << endl;
-		DestroyVulkanInstance();
 		glfwTerminate();
 		return;
 	}
 
-    glfwMakeContextCurrent(window);
+	try {
+		InitVulkan();
+	} catch (const exception& e) {
+		cerr << e.what() << endl;
+		return;
+	}
+
+	glfwMakeContextCurrent(window);
 	isManagerUp = GLFW_TRUE;
 }
 
-bool GraphicsManager::CreateVulkanInstance() {
+void GraphicsManager::InitVulkan() {
+	CreateVulkanInstance();
+	CreateVulkanSurface();
+}
+
+void GraphicsManager::CreateVulkanInstance() {
 	VkApplicationInfo appInfo{};
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Vulkan App";
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "No Engine";
-    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Vulkan App";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = "No Engine";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    VkInstanceCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
 
-    uint32_t glfwExtensionCount = 0;
-    const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+	u_int32_t extensionCount = 3;
+	const char* extensions [3] = { 
+		"VK_KHR_surface", 
+		"VK_EXT_metal_surface", 
+		"VK_KHR_portability_enumeration" 
+	}; 
 
-    createInfo.enabledExtensionCount = glfwExtensionCount;
-    createInfo.ppEnabledExtensionNames = glfwExtensions;
+	createInfo.enabledExtensionCount = extensionCount;
+	createInfo.ppEnabledExtensionNames = extensions;
+	createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
 
 	VkResult vkResult = vkCreateInstance(&createInfo, nullptr, &instance);
-    if (vkResult != VK_SUCCESS) {
-        std::cerr << "Failed to create Vulkan instance!" << std::endl;
-
+	if (vkResult != VK_SUCCESS) {
+		char* error;
 		switch (vkResult) {
 			case VK_ERROR_OUT_OF_HOST_MEMORY:
-				cerr << "Out of memory" << endl;
+				throw runtime_error("Out of memory");
 				break;
 			case VK_ERROR_OUT_OF_DEVICE_MEMORY: 
-				cerr << "Out of device memory" << endl;
+				throw runtime_error("Out of device memory");
 				break;
 			case VK_ERROR_INITIALIZATION_FAILED: 
-				cerr << "Initialization failed" << endl;
+				throw runtime_error("Initialization failed");
 				break;
 			case VK_ERROR_LAYER_NOT_PRESENT: 
-				cerr << "Layer not present" << endl;
+				throw runtime_error("Layer not present");
 				break;
 			case VK_ERROR_EXTENSION_NOT_PRESENT: 
-				cerr << "Extension not present" << endl;
+				throw runtime_error("Extension not present");
 				break;
 			case VK_ERROR_INCOMPATIBLE_DRIVER: 
-				cerr << "Incompatible driver" << endl;
+				throw runtime_error("Incompatible driver");
 				break;
 			default: 
-				cerr << "Unknown error" << endl;
+				throw runtime_error("Unknown error");
 				break;
 		}
+	}
+}
 
-		return false;
-    }
+void GraphicsManager::CreateVulkanSurface() {
+	if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
+		throw runtime_error("Failed to create VkSurface");
+	}
+}
+
+void GraphicsManager::PickPhysicalDevice() {
+	uint32_t deviceCount = 0;
+	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+	if (deviceCount) { 
+		throw runtime_error("No Vulkan-supported GPUs were found");
+	}
+
+	vector<VkPhysicalDevice> devices(deviceCount);
+	vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+	
+	for (const auto& device : devices) {
+		if (isDeviceSuitable(device)) {
+			physicalDevice = device;
+			break;
+		}
+	}
+
+	if (physicalDevice == VK_NULL_HANDLE) {
+		throw runtime_error("Failed to find suitable GPU");
+	}
+}
+
+bool GraphicsManager::isDeviceSuitable(VkPhysicalDevice device) {
+	// Maybe need to add GPU checks if needed
 	return true;
 }
 
-void GraphicsManager::DestroyVulkanInstance() {
+void GraphicsManager::CreateLogicalDevice() {
+	VkDeviceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+	VkPhysicalDeviceFeatures deviceFeatures{};
+
+	createInfo.pEnabledFeatures = &deviceFeatures;
+	createInfo.enabledExtensionCount = 0;
+
+	if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+		throw runtime_error("Failed to create logical device");
+	}
+
+	vkGetDeviceQueue(device, 0, 0, &graphicsQueue);
+}
+
+void GraphicsManager::CreateSwapChain() {
+	// TODO: what's coming here?
+}
+
+void GraphicsManager::CreateImageViews() {
+	// TODO: what's coming here?
+}
+
+void GraphicsManager::DestroyVulkan() {
+	for (auto imageView : swapChainImageViews) {
+		vkDestroyImageView(device, imageView, nullptr);
+	}
+
+	vkDestroySwapchainKHR(device, swapChain, nullptr);
+	vkDestroyDevice(device, nullptr);
+	vkDestroySurfaceKHR(instance, surface, nullptr);
 	vkDestroyInstance(instance, nullptr);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
 
 GLFWwindow* GraphicsManager::getWindow() const {
@@ -98,8 +175,6 @@ GLFWwindow* GraphicsManager::getWindow() const {
 }
 
 void GraphicsManager::destroyWindow() {
-	DestroyVulkanInstance();
-
 	cout << "Destroying main window..." << endl;
 	if (window) { 
 		glfwDestroyWindow(window);
--- GraphicsManager.h
+++ GraphicsManager.h
@@ -1,15 +1,34 @@
+#include <iostream>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+using namespace std;
 
 class GraphicsManager {
 	protected:
 	GLFWwindow* window; 
 	VkInstance instance;
+	VkSurfaceKHR surface;
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	VkDevice device;
+	VkQueue graphicsQueue;
+	VkQueue presentQueue;
+
+	VkSwapchainKHR swapChain;
+	vector<VkImage> swapChainImages;
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+	vector<VkImageView> swapChainImageViews;
 	bool isManagerUp = false;
 	
-	bool CreateVulkanInstance();
-	void DestroyVulkanInstance();
-
+	void InitVulkan();
+	void CreateVulkanInstance();
+	void CreateVulkanSurface();
+	void PickPhysicalDevice();
+	void CreateLogicalDevice();
+	void CreateSwapChain();
+	void CreateImageViews();
+	void DestroyVulkan();
+	bool isDeviceSuitable(VkPhysicalDevice device);
 
 	public: 
 	GraphicsManager(int width, int height, const char* title);
